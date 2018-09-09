449ace5949030d618f069a52b4a36dd48c19bd7e
Jack Casey <jack@MacBook-Pro.hitronhub.home>
2018-09-08 19:05:14 -0700

    improvements

--- launch.py
+++ launch.py
@@ -1,13 +1,13 @@
-#This function connects to the instagram API, then uses the Azure OCR API to give the user a pure reading instagram feed 
+#This function connects to the instagram API, then uses the Azure content recognition API to give the user a reading instagram feed 
 
 from InstagramAPI import InstagramAPI
-import getpass, http, json, urllib, base64
+import getpass, http, json, urllib, base64, time
 
 def getImageDesc(image_url):
     headers = {
     # Request headers
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '***Key For Azure***',
+    'Ocp-Apim-Subscription-Key': '##### Token #####',
     }
 
     params = urllib.parse.urlencode({
@@ -23,37 +23,57 @@ def getImageDesc(image_url):
     data = response.read()
     output = json.loads(data)
     conn.close()
-    return output
-
-
-
-username = input('Username: ')
-password = getpass.getpass(prompt='Password: ')
-print("Logging in...")
-
-API = InstagramAPI(username,password)
-API.login()
-
-API.getProfileData()
-result = API.LastJson
-
-username = result['user']['username']
-
-print("Successfully logged in as: {}".format(username))
-
-API.timelineFeed()
-timeline = API.LastJson
-timeline_posts = timeline['items']
-
-posts=[]
-
-for post in timeline_posts:
     try:
-        posts.append({
-        'username': post['user']['username'],
-        'number_likes': str(post['like_count']),
-        'caption': post['caption']['text'],
-        'image_url': post['image_versions2']['candidates'][0]['url']
-        })
-    except KeyError:
+        return output['description']['captions'][0]['text']
+    except:
         pass
+
+def login():
+    username = input('Username: ')
+    password = getpass.getpass(prompt='Password: ')
+    print("Logging in...")
+
+    global API
+    API = InstagramAPI(username,password)
+    API.login()
+
+    API.getProfileData()
+    result = API.LastJson
+    username = result['user']['username']
+
+    print("Successfully logged in as: {}".format(username))
+
+def getPosts():
+
+    API.timelineFeed()
+    timeline = API.LastJson
+    timeline_posts = timeline['items']
+
+    posts=[]
+
+    for post in timeline_posts:
+        try:
+            posts.append({
+            'username': post['user']['username'],
+            'number_likes': str(post['like_count']),
+            'caption': post['caption']['text'],
+            'image_url': post['image_versions2']['candidates'][0]['url']
+            })
+        except KeyError:
+            pass
+    return posts
+
+def printPosts(posts):
+    for post in posts:
+        print("\n \n ~ ~ ~ ~ ~ \n Loading Post... \n ~ ~ ~ ~ ~ \n \n")
+        description = getImageDesc(post['image_url'])
+        
+        time.sleep(2)
+        print("{0} posted a picture of {1}, it has {2} likes. \n".format(post['username'], description, post['number_likes']))
+        print("Caption: \n \n{0}".format(post['caption']))
+        time.sleep(0.5)
+
+login()
+
+printPosts(getPosts())
+
